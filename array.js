1b0e74c55fea23a5db5c215cc59695cc7ce0458d
TANNU <161803939+TamannaNag@users.noreply.github.com>
2024-03-15 11:26:26 +0530

    Add files via upload

--- array.js
+++ array.js
@@ -2,19 +2,19 @@
 
 /*Write a program to print all the numbers from the given array using a loop.*/
 
-const input = require("readline-sync");
+// const input = require("readline-sync");
 
-let n = input.questionInt("enter the length of array:");
+// let n = input.questionInt("enter the length of array:");
 
-let arr = []
-for(let i = 0; i<n; i++)
-{
-  arr[i] = input.question("enter the element no.:");
-} 
-for(i = 0; i<n; i++)
-{
-  console.log(arr[i]);
-}
+// let arr = []
+// for(let i = 0; i<n; i++)
+// {
+//   arr[i] = input.question("enter the element no.:");
+// } 
+// for(i = 0; i<n; i++)
+// {
+//   console.log(arr[i]);
+// }
 
 
 
@@ -23,15 +23,15 @@ for(i = 0; i<n; i++)
 /*Write a program to create an array of natural numbers till 20 and print it.*/
 
 
-const input = require("readline-sync");
-let n = input.questionInt("enter the lenght of array:");
+// const input = require("readline-sync");
+// let n = input.questionInt("enter the lenght of array:");
  
-let arr = []
-for(let i = 0; i<n; i++)
-{
- arr[i] = [i+1]
-}
-  console.log(arr);
+// let arr = []
+// for(let i = 0; i<n; i++)
+// {
+//  arr[i] = [i+1]
+// }
+//   console.log(arr);
 
 
 //question 3 ==
@@ -40,18 +40,18 @@ for(let i = 0; i<n; i++)
 
 
 
-const input = require("readline-sync");
-let n = input.questionInt("enter the lenght of array:");
+// const input = require("readline-sync");
+// let n = input.questionInt("enter the lenght of array:");
 
-let arr = []
-for(let i = 0; i<n; i++)
-{
-  arr[i] = input.question("enter the number of element no.:");
-}
-for(let i = n-1; i>=0; i--)
-{   
-console.log(arr[i]);
-} 
+// let arr = []
+// for(let i = 0; i<n; i++)
+// {
+//   arr[i] = input.question("enter the number of element no.:");
+// }
+// for(let i = n-1; i>=0; i--)
+// {   
+// console.log(arr[i]);
+// } 
 
 
 //question 4 ==
@@ -60,29 +60,29 @@ console.log(arr[i]);
 
 
 
-const input = require("readline-sync");
+// const input = require("readline-sync");
 
-let n = input.questionInt("Enter the number of elements: ");
-let arr = [];
-let c = 0;
-let t = input.questionInt("enter the target no.:");
+// let n = input.questionInt("Enter the number of elements: ");
+// let arr = [];
+// let c = 0;
+// let t = input.questionInt("enter the target no.:");
 
 
 
-for (let i = 0; i < n; i++) 
-{
-       arr[i] = input.questionInt("enter the element no. : ")
+// for (let i = 0; i < n; i++) 
+// {
+//        arr[i] = input.questionInt("enter the element no. : ")
 
-       if(arr[i]==t)
- {
-        c++;
-        i++;
-        console.log("yes");
- } else
- {
-         console.log("no");
- }
-}
+//        if(arr[i]==t)
+//  {
+//         c++;
+//         i++;
+//         console.log("yes");
+//  } else
+//  {
+//          console.log("no");
+//  }
+// }
 
 
 //question 5 ==
@@ -92,27 +92,27 @@ Write a program to print the sum of all the odd numbers and even numbers in the 
 Note: In the output, you should print odd numbers sum and even numbers sum in this order only.
 */
 
-const input = require("readline-sync");
-let n = input.questionInt("enter the length of the array:");
-let arr = [];
-let es = 0;
-let os = 0;
-for(let i = 0; i<n; i++)
-{
-   arr[i] = input.questionInt("enter the element no. : ")
-}
+// const input = require("readline-sync");
+// let n = input.questionInt("enter the length of the array:");
+// let arr = [];
+// let es = 0;
+// let os = 0;
+// for(let i = 0; i<n; i++)
+// {
+//    arr[i] = input.questionInt("enter the element no. : ")
+// }
 
-for(i =0; i<n; i++)
-{
-  if(arr[i]%2==0)
-  {
-    es = es+arr[i]
-  } else
-  {
-    os = os +arr[i]
-  }
-} console.log(es);
-  console.log(os) ;
+// for(i =0; i<n; i++)
+// {
+//   if(arr[i]%2==0)
+//   {
+//     es = es+arr[i]
+//   } else
+//   {
+//     os = os +arr[i]
+//   }
+// } console.log(es);
+//   console.log(os) ;
 
 
 
@@ -124,42 +124,42 @@ Write a program to take N numbers from a user as input and then print the duplic
 
 
 
-const input = require("readline-sync");
-let n = input.questionInt("enter the length of the number: ");
-let arr = [];
+// const input = require("readline-sync");
+// let n = input.questionInt("enter the length of the number: ");
+// let arr = [];
 
 
-for (let i = 0; i < n; i++) 
-{
-  arr[i] = input.questionInt("enter the element no.");
-}
+// for (let i = 0; i < n; i++) 
+// {
+//   arr[i] = input.questionInt("enter the element no.");
+// }
 
-for (let i = 0; i < n; i++) 
-{
-  let co = 1;
+// for (let i = 0; i < n; i++) 
+// {
+//   let co = 1;
 
   
-  for (let j = i + 1; j < n; j++)
-   {
-    if (arr[i] == arr[j]) 
-    {
-      co++;
+//   for (let j = i + 1; j < n; j++)
+//    {
+//     if (arr[i] == arr[j]) 
+//     {
+//       co++;
      
-      for (let k = j; k < n; k++)  
-      {
-        arr[k] = arr[k + 1];
-      }
-         n =n-1;
+//       for (let k = j; k < n; k++)  
+//       {
+//         arr[k] = arr[k + 1];
+//       }
+//          n =n-1;
       
-    }
+//     }
       
-  }
-    if(co>1)
-    {
-      console.log(arr[i]);
-    }
+//   }
+//     if(co>1)
+//     {
+//       console.log(arr[i]);
+//     }
     
-}
+// }
 
 
 
@@ -172,42 +172,42 @@ for (let i = 0; i < n; i++)
 
 
 
-const input = require("readline-sync");
-let n = input.questionInt("enter the length of the number: ");
-let arr = [];
+// const input = require("readline-sync");
+// let n = input.questionInt("enter the length of the number: ");
+// let arr = [];
 
 
-for (let i = 0; i < n; i++) 
-{
-  arr[i] = input.questionInt("enter the element no.");
-}
+// for (let i = 0; i < n; i++) 
+// {
+//   arr[i] = input.questionInt("enter the element no.");
+// }
 
-for (let i = 0; i < n; i++) 
-{
-  let co = 1;
+// for (let i = 0; i < n; i++) 
+// {
+//   let co = 1;
 
   
-  for (let j = i + 1; j < n; j++)
-   {
-    if (arr[i] == arr[j]) 
-    {
-      co++;
-      let k=j;
+//   for (let j = i + 1; j < n; j++)
+//    {
+//     if (arr[i] == arr[j]) 
+//     {
+//       co++;
+//       let k=j;
       
-      for (let k = j; k < n; k++) 
-      {
-        arr[k] = arr[k + 1];
-      }
-         n =n-1;
+//       for (let k = j; k < n; k++) 
+//       {
+//         arr[k] = arr[k + 1];
+//       }
+//          n =n-1;
       
-    }
-       co = co;
-  }
-    if(co>1)
-    {
-      console.log(arr[i]);
-    }
-}
+//     }
+//        co = co;
+//   }
+//     if(co>1)
+//     {
+//       console.log(arr[i]);
+//     }
+// }
 
 
 
@@ -215,49 +215,49 @@ for (let i = 0; i < n; i++)
 /*Write a program to take two sorted arrays of size M, N from the user as input and merge them into a single sorted array of size M+N (Do not use sorting)
  */
 
-const input = require("readline-sync");
+// const input = require("readline-sync");
 
-let n = input.questionInt("enter the length of the array:");
-let m = input.questionInt("enter the length of the array:");
+// let n = input.questionInt("enter the length of the array:");
+// let m = input.questionInt("enter the length of the array:");
 
-let a =[];
-let b=[];
-let d=[];
+// let a =[];
+// let b=[];
+// let d=[];
 
-for(let i=0;i<n;i++)
-{
-  a[i]=input.questionInt("enter the element num");
-}
+// for(let i=0;i<n;i++)
+// {
+//   a[i]=input.questionInt("enter the element num");
+// }
 
-for(let j=0; j<m;j++)
-{
-  b[j]=input.questionInt("enter the element num:");
-}
+// for(let j=0; j<m;j++)
+// {
+//   b[j]=input.questionInt("enter the element num:");
+// }
 
-let i =0;
-let j =0;
-let k=0;
+// let i =0;
+// let j =0;
+// let k=0;
 
-while(k+n+m)
-{
-  while(i<n)
-  {
-    while(j<m)
-    {
-      if(a[i]<b[j])
-      {
-        console.log(a[i])
-        i++;
-        k++;
-      } else
-      {
-         console.log(b[j])
-         j++;
-         k++;
-      }
-    }
-  }
-}
+// while(k+n+m)
+// {
+//   while(i<n)
+//   {
+//     while(j<m)
+//     {
+//       if(a[i]<b[j])
+//       {
+//         console.log(a[i])
+//         i++;
+//         k++;
+//       } else
+//       {
+//          console.log(b[j])
+//          j++;
+//          k++;
+//       }
+//     }
+//   }
+// }
 
 
 
@@ -275,23 +275,23 @@ array = [2, 3, 5, 2, 1]
 */
 
 
-const input = require("readline-sync");
-let n = input.questionInt("Enter the length of the array: ");
-let arr = [];
-let c = "";
+// const input = require("readline-sync");
+// let n = input.questionInt("Enter the length of the array: ");
+// let arr = [];
+// let c = "";
 
-for (let i = 0; i < n; i++) {
-  arr[i] = input.questionInt("enter the element");
-}
+// for (let i = 0; i < n; i++) {
+//   arr[i] = input.questionInt("enter the element");
+// }
 
-for (let i = 0; i < n; i++) {
-  let f = arr[i];
-  for (let j = 0; j < f; j++) {
-    c += "> ";
-  }
-  c += "\n";
-}
+// for (let i = 0; i < n; i++) {
+//   let f = arr[i];
+//   for (let j = 0; j < f; j++) {
+//     c += "> ";
+//   }
+//   c += "\n";
+// }
 
-console.log(c);
+// console.log(c);
 
 
